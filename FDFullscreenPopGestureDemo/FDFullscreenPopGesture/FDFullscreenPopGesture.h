1e03d0913b8cff69e14524218607dbeeda2c8a60
CodeEagle <stasura@hotmail.com>
2016-06-01 23:31:29 +0800

    Add Carthage Support
    Add Carthage Support

---
+++ FDFullscreenPopGestureDemo/FDFullscreenPopGesture/FDFullscreenPopGesture.h
@@ -0,0 +1,19 @@
+//
+//  FDFullscreenPopGesture.h
+//  FDFullscreenPopGesture
+//
+//  Created by LawLincoln on 16/6/1.
+//  Copyright © 2016年 forkingdog. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for FDFullscreenPopGesture.
+FOUNDATION_EXPORT double FDFullscreenPopGestureVersionNumber;
+
+//! Project version string for FDFullscreenPopGesture.
+FOUNDATION_EXPORT const unsigned char FDFullscreenPopGestureVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FDFullscreenPopGesture/PublicHeader.h>
+
+

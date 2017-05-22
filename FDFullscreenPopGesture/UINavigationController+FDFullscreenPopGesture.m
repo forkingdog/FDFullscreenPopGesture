978b3093510507d133828b2757685d4cd30e2fbd
sunjianan <sunjianan@baidu.com>
2017-05-22 16:07:41 +0800

    fix bug

--- FDFullscreenPopGesture/UINavigationController+FDFullscreenPopGesture.m
+++ FDFullscreenPopGesture/UINavigationController+FDFullscreenPopGesture.m
@@ -95,9 +95,10 @@ typedef void (^_FDViewControllerWillAppearInjectBlock)(UIViewController *viewCon
 {
     // Forward to primary implementation.
     [self fd_viewWillDisappear:animated];
+    
     dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(0 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
-        UIViewController *viewController = [self.navigationController.viewControllers lastObject];
-        if (!viewController.fd_prefersNavigationBarHidden) {
+        UIViewController *viewController = self.navigationController.viewControllers.lastObject;
+        if (viewController && !viewController.fd_prefersNavigationBarHidden) {
             [self.navigationController setNavigationBarHidden:NO animated:NO];
         }
     });

1c12eacd03f309c7706251a8fd90e0b2571c0f6c
Andy Gong <gongyujun@bytedance.com>
2017-08-10 23:17:26 +0800

    Update the gestures dependency when use the max allowed distance

--- FDFullscreenPopGesture/UINavigationController+FDFullscreenPopGesture.m
+++ FDFullscreenPopGesture/UINavigationController+FDFullscreenPopGesture.m
@@ -72,6 +72,16 @@ shouldBeRequiredToFailByGestureRecognizer:(UIGestureRecognizer *)otherGestureRec
 {
     if (gestureRecognizer == self.navigationController.fd_fullscreenPopGestureRecognizer)
     {
+        // Ignore when the beginning location is beyond max allowed initial distance to left edge.
+        UIViewController *topViewController = self.navigationController.viewControllers.lastObject;
+        CGFloat maxAllowedInitialDistance = topViewController.fd_interactivePopMaxAllowedInitialDistanceToLeftEdge;
+        
+        CGPoint beginningLocation = [gestureRecognizer locationInView:gestureRecognizer.view];
+        
+        if (maxAllowedInitialDistance > 0 && beginningLocation.x > maxAllowedInitialDistance) {
+            return NO;
+        }
+        
         return YES;
     }
     return NO;
